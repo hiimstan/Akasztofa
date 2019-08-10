ea242fa2a9b62541b00a7a8a1f1099e1c5d35213
Hegedus Krisztofer <hkriszto98@gmail.com>
2019-08-10 19:59:58 +0200

    asd

--- src/akasztofa/main/Main.java
+++ src/akasztofa/main/Main.java
@@ -1,9 +1,5 @@
 package akasztofa.main;
 
-import java.awt.EventQueue;
-
-import akasztofa.display.Display;
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,17 +8,6 @@ public class Main {
 		
 		Akasztofa akasztoFa = new Akasztofa("szavak.txt");
 		boolean jatszik = true;
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Display frame = new Display();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
 		
 		while(jatszik) {
 			akasztoFa.jatekCiklus();
